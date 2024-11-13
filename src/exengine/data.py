323c9c71d2e9417c26c165bbe6eecfb497dd088b
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-13 17:44:01 +0100

    Add `__all__`
    - make linter happy

--- src/exengine/data.py
+++ src/exengine/data.py
@@ -3,3 +3,9 @@ Convenience file for imports
 """
 from .kernel.data_coords import DataCoordinates, DataCoordinatesIterator
 from .kernel.data_handler import DataHandler
+
+__all__ = [
+    "DataCoordinates",
+    "DataCoordinatesIterator",
+    "DataHandler"
+]
--- src/exengine/notifications.py
+++ src/exengine/notifications.py
@@ -3,3 +3,9 @@ Convenience file for imports
 """
 from .kernel.notification_base import NotificationCategory
 from .kernel.notification_base import EventExecutedNotification, DataStoredNotification
+
+__all__ = [
+    "NotificationCategory",
+    "EventExecutedNotification",
+    "DataStoredNotification"
+]
