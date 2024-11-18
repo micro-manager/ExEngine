507c41f82bdce604c35202194f59ada17420be4c
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2024-11-18 08:40:56 -0800

    update analytics

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@ extensions = [
     'sphinx_togglebutton',
     'sphinx.ext.autodoc',
     'sphinx_autodoc_typehints',  # for better type hint support
+    'sphinxcontrib.googleanalytics'
 ]
 
 
@@ -48,3 +49,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+googleanalytics_id = 'G-VM4YE7745S'  # Replace with your actual GA4 measurement ID
+googleanalytics_enabled = True
